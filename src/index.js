ef9df2c94458be9c270fed392b75c4614b676648
Katrine Italyankina <chazyspoller@gmail.com>
2023-08-04 17:28:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  while (true) {
+    let currentLength = str.length;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i].join(''), '');
+    }
+    if (str.length == 0 || str.length == currentLength) {
+      break;
+    }
+  }
+  if (str.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
